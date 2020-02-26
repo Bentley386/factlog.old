ace593fd05b8f40d27936eeb742167d44f0aa7ea
bor brecelj <bor.brecelj@gmail.com>
2020-02-26 13:14:37 +0100

    Move parameters to script arguments with argparse

--- pca_on_range.py
+++ pca_on_range.py
@@ -2,8 +2,7 @@
 A script for performing PCA on a time range of JEMS sensor data and dumping the
 results to disk.
 
-Before running the script, you have to set START_DATE, END_DATE and
-SAVE_LOCATION.
+You can set start_date(-s), end_date(-e) and save_location(-l) with arguments.
 
 First it queries sensor values from the database. Then data is transformed to
 the correct format and missing values are replaced with mean value. Results
@@ -15,6 +14,7 @@ For 1 year of data, the script runs a few minutes.
 
 import os
 import pyodbc
+import argparse
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
@@ -29,43 +29,51 @@ else:
 
 from jems_data import DieselDs
 
-# TODO: move parameters to script arguments with argparse
-# start and end of interval to analyse
-START_DATE = "2017-01-21 00:00:00.0"
-END_DATE = "2017-01-22 00:00:00.0"
-# where to save results (must end with '/')
-SAVE_LOCATION = "./"
 
 def write_to_table(row, table):
     """Write a sensor row to a table."""
     table.loc[row['timestamp'], row['sensors_id']] = row['value']
 
+
 if __name__ == "__main__":
+    # read arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--start-date', default="2017-01-21", help="Start of interval to analyse. \
+                        Date must be given as 'YYYY-MM-DD hh:mm:ss' or as any prefix of it.", metavar='S')
+    parser.add_argument('-e', '--end-date', default="2017-01-22", help="End of interval to analyse. \
+                        Date must be given as 'YYYY-MM-DD hh:mm:ss' or as any prefix of it.", metavar='E')
+    parser.add_argument('-l', '--save-location', default="results/", help="Location for saving results", metavar='L')
+    args = parser.parse_args()
+
+    # check if save location is a valid path
+    if not os.path.exists(args.save_location):
+        raise RuntimeError("Save location must be a valid path.")
+
     # initialize Diesel data source
     dt = DieselDs(os.getenv("DB_PASSWORD"))
 
-    # get sensor values for each hour between START_DATE and END_DATE
-    values = dt.load_range(START_DATE, END_DATE, "hour")
+    # get sensor values for each hour between start and end date
+    values = dt.load_range(args.start_date, args.end_date, "hour")
 
     # create dataframe where each column represents one sensor
     table = pd.DataFrame(index = values["timestamp"].unique(), columns = values["sensors_id"].unique())
     values.apply(lambda row: write_to_table(row, table) , axis=1)
-    table.to_csv(SAVE_LOCATION + 'sensor_values.csv')
+    table.to_csv(os.path.join(args.save_location, 'sensor_values.csv'))
 
     # create dataframe where NaN are replaced by mean value of a column
     imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
     newTable = pd.DataFrame(data = imp_mean.fit_transform(table),
                             index = values["timestamp"].unique(),
                             columns = values["sensors_id"].unique())
-    newTable.to_csv(SAVE_LOCATION + 'sensor_values_without_nan.csv')
+    newTable.to_csv(os.path.join(args.save_location, 'sensor_values_without_nan.csv'))
 
     # PCA
     pca = PCA()
     pca.fit(newTable)
 
     # save components and singular values
-    np.save(SAVE_LOCATION + 'components', pca.components_)
-    np.save(SAVE_LOCATION + 'singular_values', pca.singular_values_)
+    np.save(os.path.join(args.save_location, 'components'), pca.components_)
+    np.save(os.path.join(args.save_location, 'singular_values'), pca.singular_values_)
 
     # print information about the sensor which has the biggest absolute value of coefficient
     # in vector that points in direction of the biggest variance
