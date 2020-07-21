dde6f94e6c022fa155578912e1c2c743d1ec3d78
bor brecelj <bor.brecelj@gmail.com>
2020-07-21 14:36:17 +0200

    Add prediction of next state

--- transitionModel.py
+++ transitionModel.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from skmultiflow.trees import HoeffdingTreeClassifier
 from skmultiflow.data import DataStream
+from typing import List
 
 
 class TransitionModel:
@@ -34,11 +35,14 @@ class TransitionModel:
     def check_data(self):
         pass
 
-    def prepare_data(self, data: pd.DataFrame) -> DataStream:
-        """ Take raw data and return data stream with running average and running delta. """
-        data = pd.concat([self.history, data])
-        # Update self.history to have last self.window_size measurements
-        self.history = data.tail(self.window_size)
+    def prepare_data(self, data: pd.DataFrame, drop_last_row: bool = True, use_history: bool = True) -> pd.DataFrame:
+        """ Take raw data and return data stream with running average and running delta. For the last row there is no
+            next state, so drop_last_row should be True for learning, but False for predicting. If use_history is set
+            to true function will add history to the data and update history."""
+        if use_history:
+            data = pd.concat([self.history, data])
+            # Update self.history to have last self.window_size measurements
+            self.history = data.tail(self.window_size)
 
         sensor_values = data.drop(columns='label')
         labels = data['label']
@@ -51,14 +55,15 @@ class TransitionModel:
         prepared_data['next_state'] = labels.shift(periods=-1, fill_value=-1)
 
         # Drop last row, because we don't know next state yet
-        prepared_data.drop(prepared_data.tail(1).index, inplace=True)
+        if drop_last_row:
+            prepared_data.drop(prepared_data.tail(1).index, inplace=True)
         prepared_data.drop(prepared_data.head(self.window_size-1).index, inplace=True)
-        return DataStream(prepared_data)
+        return prepared_data
 
     def partial_fit(self, data: pd.DataFrame) -> None:
         """ The most basic working version for now.
         TODO: improve, calculate accuracy, maybe add possibility to learn in batches? """
-        stream = self.prepare_data(data)
+        stream = DataStream(self.prepare_data(data))
         n = stream.n_remaining_samples()
         # Test
         # count_right = 0
@@ -88,8 +93,16 @@ class TransitionModel:
         # Same: 8715 (number of times a state stays the same)
         # Success: 125 (number of correct predictions when state changed)
 
-    def predict(self):
-        pass
+    def predict(self, data: pd.DataFrame = pd.DataFrame(), use_history: bool = True) -> List[int]:
+        """ Argument data is a DataFrame with shape (n_samples, n_features).
+        use_history tells whether or not history will be included in data before making prediction. If use_history is
+        set to true, function will make n_samples+1 predictions, otherwise n_samples-window_size+1 predictions."""
+
+        if not use_history and data.shape[0] < self.window_size:
+            raise RuntimeError("Not enough measurements to make a prediction.")
+        prepared_data = self.prepare_data(data, drop_last_row=False, use_history=use_history)
+        prepared_data.drop(columns="next_state", inplace=True)
+        return self.model.predict(prepared_data.values)
 
     def save_model(self):
         pass
@@ -98,4 +111,7 @@ class TransitionModel:
 if __name__ == '__main__':
     data = pd.read_csv("../stateGraphOutput.csv", index_col=0)
     tm = TransitionModel(5)
-    tm.partial_fit(data)
+    tm.partial_fit(data[:-10])
+    print(tm.predict(data[-10:]))
+    # print(tm.predict(data[-10:], use_history=False))
+    # tm.predict(data[-1:], use_history=False)  # Runtime error
