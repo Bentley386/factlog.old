399302f7363be91493b81ae2f7599c0847792696
Admin <spyro386@gmail.com>
2020-08-24 04:19:38 +0200

    Added histograms to the Visualization module.

--- stream_story_py/visualization.py
+++ stream_story_py/visualization.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from plotly.offline import plot
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
@@ -26,12 +27,12 @@ class Visualization():
         """
         
         self.graph = StateGraph(n_clusters)
-        data = self.graph.fit_transform(raw_data)
+        self.data = self.graph.fit_transform(raw_data)
         self.norm_data = pd.DataFrame(
                 data=self.graph.normalisation.transform(raw_data),
                 index=raw_data.index,
                 columns=raw_data.columns)
-        self.labels = np.array([data["label"].to_numpy()]).T
+        self.labels = np.array([self.data["label"].to_numpy()]).T
         
         
     def get_parallel_plot(self) -> go.Figure:
@@ -70,11 +71,38 @@ class Visualization():
                 np.concatenate((tsne_result,self.labels),axis=1),
                 columns=["tsne_x","tsne_y","cluster"])
         return px.scatter(plot_data,x="tsne_x",y="tsne_y",color="cluster")
+    
+    def get_histograms(self,cluster : int) -> go.Figure:
+        """Returns a figure containing a histogram for each sensor 
+        for the data in a given cluster (cluster index starts at 0).
+        """
         
+        maxcluster = np.amax(self.labels)
+        if cluster > maxcluster:
+            raise ValueError(f"cluster must be between 0 and {maxcluster}")
+            
+        filtered = self.data[self.data["label"] == cluster]
+        cols = filtered.columns.values[:-1]
+        
+        fig = make_subplots(rows=len(cols)//2,cols=2,subplot_titles=cols)
+        for index, sensor in enumerate(cols):
+            x = filtered[sensor].values
+            fig.append_trace(
+                    go.Histogram(x=x, name=sensor),
+                    row=index//2+1,
+                    col=index%2+1
+                    )
+            fig.update_xaxes(title_text=sensor, row=index//2+1, col=index%2+1)
+            fig.update_yaxes(title_text="Count", row=index//2+1, col=index%2+1)
+
+        fig.update_layout(autosize=False,width=1500,height=200*len(cols),
+                          showlegend=False)
+        return fig
         
 if __name__ == "__main__":
     sensor_list = ["50", "53", "55", "62", "63", "64", "65", "97", "98"]
     sensor_values = pd.read_csv(open('../data/B100_hour_SS_input.csv'), index_col=0)
     values = sensor_values.filter(items=["timestamp"] + sensor_list)
-    visual = Visualization(values,5) 
-    plot(visual.get_PCA(),auto_open=True)
+    visual = Visualization(values,5)
+    #visual.get_histograms(2)
+    plot(visual.get_histograms(2),auto_open=True)
