d823acf200a794b98bfba4971b0089f4ecfd3565
Klemen Kenda <klemen.kenda@ijs.si>
2020-09-14 23:41:32 +0200

    Adding init files

--- exploratory_analysis/src/__init__.py
+++ exploratory_analysis/src/__init__.py
@@ -1,7 +1,3 @@
 # import submodules here
-
-import sys
-sys.path.insert(0, './data')
-sys.path.insert(0, './features')
-
-from jems_data import *
+from . import data
+from . import features
---
+++ exploratory_analysis/src/data/__init__.py
@@ -0,0 +1,4 @@
+""" A data manipulation module
+"""
+from .jems_data import DieselDs
+from .jems_data import reshape_sensor_data
